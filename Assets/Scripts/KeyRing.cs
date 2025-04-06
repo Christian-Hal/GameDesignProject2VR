a74f81bd028d10b9ced1a207c28df63f24fcf962
ChicknNWaffles <elora.browning@gmail.com>
2025-04-05 21:22:30 -0500

    Keyring state machine completed and functinoal
    I now need to hook it up to the animations that the designer made

--- Assets/Scripts/KeyRing.cs
+++ Assets/Scripts/KeyRing.cs
@@ -40,31 +40,55 @@ public class KeyRing : MonoBehaviour
 
     public void Flip() {
         print("Flipping keyring");
+        if (curState == State.Held) {
+            print("going to key 1 from held");
+            ChangeState(State.Key1);
+        }else if (curState == State.Key1) {
+            print("going to key 2 from key 1");
+            ChangeState(State.Key2);
+        }else if (curState == State.Key2) {
+            print("going to key 3 from key 2");
+            ChangeState(State.Key3);
+        }else if (curState == State.Key3){
+            print("going to key 1 from key 3");
+            ChangeState(State.Key1);
+        } else{
+            print("error. Current state is " + curState + " going nowhere");
+        }
     }
 
     public void PickUp(){
         print("Picking up keys");
+        if(curState == State.Idle) {
+            ChangeState(State.Held);
+        }
     }
 
     public void PutDown() {
         print("Putting down keys");
+        ChangeState(State.Idle);
     }
 
     #region state enter methods
     private void StateEnterIdle() {
-        
+        print("play animation to lower all keys");
+        print("play jingling sound");
     }
     private void StateEnterHeld(){
-
+        print("play animation to lower all keys");
+        print("play jingling sound");
     }
     private void StateEnterKey1(){
-
+        print("play animation to raise key 1");
+        print("play jingling sound");
     }
     private void StateEnterKey2(){
-
+        print("play animation to raise key 2");
+        print("play jingling sound");
     }
     private void StateEnterKey3(){
-
+        print("play animation to raise key 3");
+        print("play jingling sound");
     }
     #endregion
 
