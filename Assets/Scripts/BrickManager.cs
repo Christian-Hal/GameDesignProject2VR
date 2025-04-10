eb6d0d7da01d490009a0ee4bd1d1cb6855ee74a6
Gavin Dominique <gavindom315@gmail.com>
2025-04-09 21:33:06 -0500

    BRICK PUZZLE DONE

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -14,21 +14,31 @@ public class BrickManager : MonoBehaviour
     public bool five;
     private bool all;
     public GameObject door;
+
+    private float openTimer;
     // Start is called before the first frame update
     void Start()
     {
         all = false;
+        openTimer = float.MaxValue;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        openTimer -= Time.deltaTime;
+
         if (one == true && two == true && three == true && four == true && five == true && all == false)
         {
             all = true;
             print("ALL ARE CORRECT");
-            door.GetComponent<Animator>().SetTrigger("OpenDoor");
-            //door.GetComponent<Animation>().Play();
+            openTimer  = 1.5f;
+        }
+
+        if (all == true && openTimer <= 0)
+        {
+            door.GetComponent<Animator>().SetTrigger("DoorOpen");
+            openTimer = float.MaxValue;
         }
     }
 
