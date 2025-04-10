1cbb750150640337e04a3f3bab51f1e8b3a321fb
Lily <ndd232@gmail.com>
2025-04-09 19:22:44 -0500

    Implemented my puzzle (mostly) into the main scene

---
+++ Assets/Scripts/SwordSnapZones.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Utilities;
+
+public class SwordSnapZones : MonoBehaviour
+{
+    public GameObject TargetObject; // Object that can trigger the snap zone
+    private MeshRenderer meshRenderer;
+    public Color hoverColor = Color.green; // Color when object hovers
+    private Color originalColor;
+    public GameObject manager;
+    public int swordNum;
+
+    void Start()
+    {
+        // Get the MeshRenderer component and store the original color
+        meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            originalColor = meshRenderer.material.color;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        ChangeColor(hoverColor); // Change to hover color
+        if (other.gameObject == TargetObject) // Ensure it's the correct object
+        {
+            Debug.Log("Snap Zone Entered by TargetObject"); 
+            manager.GetComponent<SwordManager>().correctSword(swordNum);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        ChangeColor(originalColor); // Revert to original color
+        if (other.gameObject == TargetObject)
+        {
+            Debug.Log("Snap Zone Exited by TargetObject");
+           manager.GetComponent<SwordManager>().correctSword(swordNum);
+        }
+    }
+
+    void ChangeColor(Color newColor)
+    {
+        if (meshRenderer != null)
+        {
+            meshRenderer.material.color = newColor;
+        }
+    }
+}
