a456eac89e6ac0dc7a3335887fa208fa2a571132
Karkaton-V <125698830+Karkaton-V@users.noreply.github.com>
2025-04-10 14:56:16 -0500

    fixed shelf bsery

--- Assets/Scripts/playMonocleJingle.cs
+++ Assets/Scripts/playMonocleJingle.cs
@@ -17,14 +17,15 @@ public class playMonocleJingle : MonoBehaviour
     {
         if (Dollhouse.DollhouseComplete == true) 
         {
-            playOnce();
+            playOnceAndMove();
         }
     }
 
-    void playOnce()
+    void playOnceAndMove()
     {
         if (repeat == 0)
         {
+            this.transform.position = new Vector3(4, 0, 0);
             jingle.Play();
             repeat += 1;
         }
